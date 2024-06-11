44d0116abcf4bccf30482fc436a2fd44369d29a2
tb0902 <tbruton417@gmail.com>
2024-06-11 13:13:59 -0400

    Added two new roles, Teacher and Student successfully through Liquibase

--- src/main/webapp/app/entities/reducers.ts
+++ src/main/webapp/app/entities/reducers.ts
@@ -1,6 +1,8 @@
+import reportCard from 'app/entities/report-card/report-card.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
+  reportCard,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
+import ReportCard from './report-card';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default () => {
@@ -10,6 +11,7 @@ export default () => {
     <div>
       <ErrorBoundaryRoutes>
         {/* prettier-ignore */}
+        <Route path="report-card/*" element={<ReportCard />} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </ErrorBoundaryRoutes>
     </div>
